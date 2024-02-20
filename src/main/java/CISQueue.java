4126b8342d57fb70fcf0ccf48d8d4abb088d6abb
ericrcwu001 <ericrw2025@student.cis.edu.hk>
2024-02-20 10:01:11 +0800

    queue done

--- src/main/java/CISQueue.java
+++ src/main/java/CISQueue.java
@@ -4,25 +4,43 @@ public class CISQueue<T> {
     // Use your Linked List class that you created in Lab 1!
     // Thought 1: is aggregation or composition going to be used here?
     // Thought 2: meditate on the word 'abstraction'.
+    CISLinkedList<T> ll;
 
     // Our CISQueue class will have a constructor.
     // Our CISQueue constructor will instantiate a Queue of size 1.
-
+    public CISQueue(T val) {
+        ll = new CISLinkedList<>(val);
+    }
     // isEmpty
     // Returns a boolean.
     // Indicates whether the Queue is empty.
+    public boolean isEmpty() {
+        return ll.isEmpty();
+    }
 
     // size
     // Returns an integer.
     // Indicates how many elements are currently in our Queue.
+    public int size() {
+        return ll.size();
+    }
 
     // dequeue
     // Returns the data stored in the node that will be polled.
+    public T dequeue() {
+        return ll.removeFirst();
+    }
 
     // enqueue
     // Adds a node to the end of our Queue.
+    public void enqueue(T val) {
+        ll.addLast(val);
+    }
 
     // toString
     // Returns a String representation of our Queue.
+    public String toString() {
+        return ll.toString();
+    }
 
 }
